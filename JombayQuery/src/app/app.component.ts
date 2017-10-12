e7a594dacc6fc534884be4d89e2277a60f0e5243
pankajz <pankajszade@gmail.com>
2017-10-12 11:43:28 +0530

    hide unwanted

--- JombayQuery/src/app/app.component.ts
+++ JombayQuery/src/app/app.component.ts
@@ -9,63 +9,11 @@ import { StorageService } from './storage.service';
 })
 export class AppComponent {
   title = 'Jombays';
-  // authResp:any;
-  // usrResp:any;
-  // queResp:any;
-  // queArr:any;
-  // nextKey:any;
-  // prevKey:any[]=[];
   
-  // private handleError(error: any): Promise<any> {
-  //   console.error('An error occurred', error); 
-  //   return Promise.reject(error.message || error);
-  // }
   constructor(public webservicesService:WebservicesService, public storageService:StorageService) { 
-    // this.queArr = [];
-    // this.getAuthToken();
-    //this.getCaseStudy('');
+  
   }
-  //Get auth token
-  // getAuthToken(){
-  //   this.webservicesService.getAuthTokenService()
-  //   .then(data=>{
-  //     this.authResp = data;
-  //     this.storageService.tokenStore(this.authResp);
-  //     this.getCaseStudy();
-  //     this.getCurrentUser();
-  //   })
-  //   .catch(this.handleError)
-  // }
-
-  // //Get Current User
-  // getCurrentUser(){
-  //   let tokenStore = this.storageService.tokenStore('');
-    
-  //   let token=tokenStore;
-  //   //console.log(token);
-  //   this.webservicesService.getCurrentUserService(token)
-  //   .then(data=>{
-  //     this.usrResp = data;
-  //   })
-  //   .catch(this.handleError)
-  // }
-
-  //  //Get Case Study
-  // getCaseStudy(){
-  //   let tokenStore = this.storageService.tokenStore('');
-    
-  //   let token=tokenStore;
-  //   this.webservicesService.getQuestionsService(token)
-  //   .then(data=>{
-  //     this.queResp = data;
-  //     let que = this.queResp.user_company_case_study.company_case_study.questions;
-  //     for(var i=0; i<que.length; i++){
-  //       this.queArr.push({'id':i,'question':que[i].body,'answer':""});
-  //     }
-  //     console.log(this.queArr);
-  //   })
-  //   .catch(this.handleError)
-  // }
+  
 }
 
 
